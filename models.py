bef145c187a27de097533b87629582ce6491f979
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2025-08-12 01:45:16 -0400

    Update models.py

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ class Entry(BaseModel):
     title: constr(strip_whitespace=True, min_length=1, max_length=100)
     content: constr(strip_whitespace=True, min_length=1, max_length=2000)
     edit_code: constr(min_length=6, max_length=64)
-    type: EntryType
+    entry_type: EntryType = Field(..., alias="type")
     unique_name: str | None = None
     
 class DeleteEntry(BaseModel):
